624bb5ac59846a13ec431d3260b83567fb4db90d
KyaBacchaHai <55993921+KyaBacchaHai@users.noreply.github.com>
2019-10-28 21:06:39 +0530

    Update

--- searchingAlgo/binarySearch/BinarySearch.c
+++ searchingAlgo/binarySearch/BinarySearch.c
@@ -1,30 +1,43 @@
-int BinarySearch(int Arr[], int i, int f){
-
-}
+#include<stdio.h>
+ 
+//The below code is an algorithm implementing Binary search
+//	It uses a while loop and has a time complexity of 
+//	O(N) = log(N)
+//	Θ(N) = log(N)
+//	Ω(N) = 1 ( constant )
+		
 
 int main(){
 
-	Arr[] = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 } 
+	int start, end, mid, ans;
 
-
+	int Arr[] = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+	int X = 0 ;
 
 
 	start = 0;  //INITIALISE
-	end = ;   // SIZE OF ARRAY
-	mid = (start + end)/2; // INITIALISE 
+	end = 10 ;   // SIZE OF ARRAY
 	ans = -1;  // IF ELEMENT IS NOT FOUND -1 WOULD BE PRINTED
-	while( start > end ){
+
+	while( start <= end ){
+
+		mid = (start + end)/2; // INITIALISE 
+
 
 		if( Arr[mid] == X){
 			ans = mid+1;
 			break;
 		}
-		if(Arr[mid] > X)
+
+		else if(Arr[mid] > X)
 			end = mid-1;
+
 		else
 			start = mid +1;
+
+
 	}
 
-	printf("%d", ans)
+	printf("%d\n", ans);
 	return 0;
 }
