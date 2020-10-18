4fcd30ead020be1584fe5585193f939833af4f1c
prerna250 <72258930+prerna250@users.noreply.github.com>
2020-10-18 21:07:03 +0530

    Update combSort.py

--- sortingAlgo/combSort/combSort.py
+++ sortingAlgo/combSort/combSort.py
@@ -1,49 +1,40 @@
+'''Comb Sort is mainly an improvement over Bubble Sort. Bubble sort always compares
+adjacent values. So all inversions are removed one by one. Comb Sort improves on Bubble
+Sort by using gap of size more than 1. The gap starts with a large value and shrinks by
+a factor of 1.3 in every iteration until it reaches the value 1. Thus Comb Sort removes
+more than one inversion counts with one swap and performs better than Bubble Sort.'''
+
+
 # Python program for implementation of CombSort 
 
-# To find next gap from current 
-def getNextGap(gap): 
 
-	# Shrink gap by Shrink factor 
-	gap = (gap * 10)/13
-	if gap < 1: 
-		return 1
-	return gap 
-
-# Function to sort arr[] using Comb Sort 
-def combSort(arr): 
-	n = len(arr) 
-
-	# Initialize gap 
-	gap = n 
-
-	# Initialize swapped as true to make sure that 
-	# loop runs 
-	swapped = True
-
-	# Keep running while gap is more than 1 and last 
-	# iteration caused a swap 
-	while gap !=1 or swapped == 1: 
-
-		# Find next gap 
-		gap = getNextGap(gap) 
-
-		# Initialize swapped as false so that we can 
-		# check if swap happened or not 
-		swapped = False
-
-		# Compare all elements with current gap 
-		for i in range(0, n-gap): 
-			if arr[i] > arr[i + gap]: 
-				arr[i], arr[i + gap]=arr[i + gap], arr[i] 
-				swapped = True
-
-
-# Driver code to test above 
-arr = [ 8, 4, 1, 3, -44, 23, -6, 28, 0] 
-combSort(arr) 
-
-print ("Sorted array:") 
-for i in range(len(arr)): 
-	print (arr[i]), 
-
-
+def comb_sort(alist):
+    def swap(i, j):
+        alist[i], alist[j] = alist[j], alist[i]
+ 
+    gap = len(alist)
+    shrink = 1.3
+ 
+    no_swap = False
+    while not no_swap:
+        gap = int(gap/shrink)
+ 
+        if gap < 1:
+            gap = 1
+            no_swap = True
+        else:
+            no_swap = False
+ 
+        i = 0
+        while i + gap < len(alist):
+            if alist[i] > alist[i + gap]:
+                swap(i, i + gap)
+                no_swap = False
+            i = i + 1
+ 
+#TEST CODE 
+alist = input('Enter the list of numbers separated by space: ').split()
+alist = [int(x) for x in alist]
+comb_sort(alist)
+print('Sorted list: ', end='')
+print(alist)
