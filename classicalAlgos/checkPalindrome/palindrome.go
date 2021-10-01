1654a1310889f0f1d7a0b0fc083a1be56d8ceb95
Pahulpreet Singh <54016648+codelixir@users.noreply.github.com>
2021-10-02 01:20:40 +0530

    added comments

--- classicalAlgos/checkPalindrome/palindrome.go
+++ classicalAlgos/checkPalindrome/palindrome.go
@@ -8,14 +8,17 @@ func main() {
 
 	var str_in string
 	fmt.Scanln(&str_in)
+	// take user input
 
 	n := len([]rune(str_in))
 	str_check := ""
 
+	// reversing the input string and storing it in str_check
 	for i := n - 1; i >= 0; i-- {
 		str_check += string(str_in[i])
 	}
 
+	// if string == its reverse, it's a palindrome
 	if str_check == str_in {
 		fmt.Println("Palindrome.")
 	} else {
