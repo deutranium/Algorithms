ec13a6f17db044e4d4420077a56f4f320a50850e
Pahulpreet Singh <54016648+codelixir@users.noreply.github.com>
2021-10-11 09:42:33 +0530

    Update binarySearchRecursive.cpp
    Indentation

--- searchingAlgo/binarySearch/binarySearchRecursive.cpp
+++ searchingAlgo/binarySearch/binarySearchRecursive.cpp
@@ -4,28 +4,28 @@ using namespace std;
  bool binse(int arr[],int l,int h,int x)
  {
      if(l>h)
-     return -1;
+     	return -1;
      int mid=l+(h-l)/2; //causes (l+h)/2 overloading so l+(h-l)/2 is preferred 
      if(arr[mid]==x)
-     return mid;
+     	return mid;
      if(arr[mid]>x)
-     return binse(arr,l,mid-1,x);
+     	return binse(arr,l,mid-1,x);
      else 
-     return binse(arr,mid+1,h,x);
+     	return binse(arr,mid+1,h,x);
  }
 int main() {
     int n;
     cin>>n;
     int arr[n];
     for(int i=0;i<n;i++)
-    cin>>arr[i];
+    	cin>>arr[i];
     int x;
     cin>>x;//number to be searched
     int l=0,h=n-1;//lower limit and higher limit
     int pos=binse(arr,l,h,x);
     if(pos>=0)
-    cout<<"The element is found at "<<pos+1;
+    	cout<<"The element is found at "<<pos+1;
     else 
-    cout<<"The element was not found";
-	return 0;
+    	cout<<"The element was not found";
+    return 0;
 }
