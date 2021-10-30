9464a36fc00c96dc41682662cef5414839b58ba8
Keshav Bajaj <keshav.bajaj@research.iiit.ac.in>
2021-10-30 14:46:03 +0530

    Added Sieve of Eratosthenes in C

---
+++ primalityTest/Sieve of Eratosthenes/SieveOfEratosthenes.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+
+int main()
+{
+    int number = 100, i, j;
+    int primes[number + 1];
+
+    for (i = 2; i <= number; i++) //populating array with naturals numbers
+        primes[i] = i;
+
+    i = 2;
+    while ((i * i) <= number)
+    {
+        if (primes[i] != 0)
+        {
+            for (j = 2; j < number; j++)
+            {
+                if (primes[i] * j > number)
+                    break;
+                else
+                    primes[primes[i] * j] = 0; // Instead of deleteing , making elemnets 0
+            }
+        }
+        i++;
+    }
+
+    int result[number + 1];
+    int k = 0;
+    for (i = 2; i <= number; i++)
+    {
+        //If number is not 0 then it is prime
+        if (primes[i] != 0)
+        {
+            result[k++] = primes[i];
+        }
+    }
+
+    return 0;
+}
+
+/*
+TIme Complexity : O(nlog(logn))
+Space Complexity : O(n)
+*/
