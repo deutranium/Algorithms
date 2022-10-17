a277fb7aa99ffa379d5066d4276e0194e6da7b8c
areesha <areeshasayed09@gmail.com>
2022-10-17 19:46:38 +0530

    Kruskals algorithm in python

--- classicalAlgos/kruskalsAlgorithm/kruskals.py
+++ classicalAlgos/kruskalsAlgorithm/kruskals.py
@@ -1,42 +1,55 @@
+
+
+# Class to represent a graph
+
+
 class Graph:
 
 	def __init__(self, vertices):
-		self.V = vertices 
+		self.V = vertices # No. of vertices
 		self.graph = []
-		
+		# to store graph
 
-	
+	# function to add an edge to graph
 	def addEdge(self, u, v, w):
 		self.graph.append([u, v, w])
 
-	
+	# A utility function to find set of an element i
+	# (truly uses path compression technique)
 	def find(self, parent, i):
 		if parent[i] != i:
-		
+		# Reassignment of node's parent to root node as
+		# path compression requires
 			parent[i] = self.find(parent, parent[i])
 		return parent[i]
 
-	
+	# A function that does union of two sets of x and y
+	# (uses union by rank)
 	def union(self, parent, rank, x, y):
 		
-		
+		# Attach smaller rank tree under root of
+		# high rank tree (Union by Rank)
 		if rank[x] < rank[y]:
 			parent[x] = y
 		elif rank[x] > rank[y]:
 			parent[y] = x
 
-		
+		# If ranks are same, then make one as root
+		# and increment its rank by one
 		else:
 			parent[y] = x
 			rank[x] += 1
 
-	
+	# The main function to construct MST using Kruskal's
+		# algorithm
 	def KruskalMST(self):
 
 		result = [] 
+
+		# An index variable, used for sorted edges
 		i = 0
 
-		
+		# An index variable, used for result[]
 		e = 0
 
 		
@@ -46,12 +59,12 @@ class Graph:
 		parent = []
 		rank = []
 
-		
+		# Create V subsets with single elements
 		for node in range(self.V):
 			parent.append(node)
 			rank.append(0)
 
-		
+		# Number of edges to be taken is equal to V-1
 		while e < self.V - 1:
 
 			
@@ -65,7 +78,7 @@ class Graph:
 				e = e + 1
 				result.append([u, v, w])
 				self.union(parent, rank, x, y)
-			
+			# Else discard the edge
 
 		minimumCost = 0
 		print("Edges in the constructed MST")
@@ -75,7 +88,7 @@ class Graph:
 		print("Minimum Spanning Tree", minimumCost)
 
 
-
+# Driver's code
 if __name__ == '__main__':
 	g = Graph(4)
 	g.addEdge(0, 1, 10)
@@ -84,7 +97,7 @@ if __name__ == '__main__':
 	g.addEdge(1, 3, 15)
 	g.addEdge(2, 3, 4)
 
-	
+	# Function call
 	g.KruskalMST()
 
 
